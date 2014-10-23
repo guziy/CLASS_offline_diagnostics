ccbbeaf3be7e8c50c9517cc071eb1096aa6d18f2
huziy <guziy.sasha@gmail.com>
2014-10-23 15:44:38 -0400

    start the project, to do: do the visualizations for a given exp

---
+++ plot_monthly_exp_data.py
@@ -0,0 +1,24 @@
+__author__ = 'huziy'
+
+
+varname_to_nlevs = {
+    "TBAR": 3,
+    # "SNO": 1
+}
+
+
+def do_seasonal_2d_plots(exp_name="", data_folder=""):
+    """
+    :param exp_name: name of the experiment
+    """
+    for vname, nlevs in varname_to_nlevs:
+
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
